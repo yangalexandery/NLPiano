66a163517fbd201b0867a31ab525e85cc8ae5211
Alexander Yang <yangalexandery@gmail.com>
2017-09-19 02:16:29 +0000

    dealing with mismatching dimensions in current model code

--- rnn_model.py
+++ rnn_model.py
@@ -16,13 +16,14 @@ input_size = 78
 state_size = 100
 
 sample_length = 128
+num_substates = 2
 
 def create_model():
-    batchX_placeholder = tf.placeholder(tf.float32, [batch_size, truncated_backprop_length])
-    batchY_placeholder = tf.placeholder(tf.int32, [batch_size, truncated_backprop_length])
+    batchX_placeholder = tf.placeholder(tf.float32, [batch_size, sample_size, input_size, num_substates])
+    batchY_placeholder = tf.placeholder(tf.int32, [batch_size, sample_size, input_size, num_substates])
 
     init_state = tf.placeholder(tf.float32, [batch_size, state_size])
-    U = tf.Variable(np.random.rand(input_size, state_size)) # weights for input -> state t
+    U = tf.Variable(np.random.rand(num_substates*input_size, state_size)) # weights for input -> state t
     W = tf.Variable(np.random.rand(state_size, state_size)) # weights for state t-1 -> state t
     b = tf.Variable(np.zeros((1, state_size)), dtype=tf.float32)
 
@@ -36,23 +37,26 @@ def create_model():
     # forward pass
     current_state = init_state
     states_series = []
-    for current_input in inputs_series:
-        current_input = tf.reshape(current_input, [batch_size, 1])
-        # input_and_state_concatenated = tf.concat(1, [current_input, current_state])  # Increasing number of columns
+    for current_input in inputs_series: # there should be sample_size number of inputs, which is correct.
+        # current_input = tf.reshape(current_input, [batch_size, 1]) # old code -- commented out for now because idk if this is correct
+        current_input = tf.reshape(current_input, [batch_size, num_substates*input_size]) # let's just use this for now to see if it works
 
         next_state = tf.tanh(tf.matmul(current_input, U) + tf.matmul(current_state, W)+ b) # s_t = f(U x_t + W s_t-1)
+        # we want each state to be a vector of shape [state_size, 1]
+        # in this case [batch_size, state_size, 1]
         states_series.append(next_state)
         current_state = next_state
 
 
+    # TODO: figure out if this stuff actually works
     logits_series = [tf.matmul(state, V) + b_V for state in states_series] #Broadcasted addition
     predictions_series = [tf.nn.softmax(logits) for logits in logits_series]
 
     losses = [tf.nn.sparse_softmax_cross_entropy_with_logits(logits, labels) for logits, labels in zip(logits_series,labels_series)]
     total_loss = tf.reduce_mean(losses)
+    # TODO: see if matrix multiplications can be optimized by using tensorflow's a_is_sparse or b_is_sparse flag.
+    train_step = tf.train.AdagradOptimizer(0.3).minimize(total_loss)
 
-
-train_step = tf.train.AdagradOptimizer(0.3).minimize(total_loss)
 def learn_things():
     midi_list = midi_io.get_pieces()
     for i in range(0, 20):
