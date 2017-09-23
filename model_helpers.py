e4771201b2ea456c29a38e33560a588431afc99e
Alexander Yang <yangalexandery@gmail.com>
2017-09-23 02:54:11 +0000

    wrote method to convert statematrix to feature matrix

--- model_helpers.py
+++ model_helpers.py
@@ -37,3 +37,42 @@ def get_sample_output(sample):
     input_size = len(sample[0])
     return np.vstack((sample_output, np.array([[[0.0, 0.0] for i in range(input_size)]])));
     # return sample_output;
+
+def transform_statematrix(statematrix):
+    # given statematrix, return feature matrix
+    new_feature_matrix = []
+    feature_vector_length = 79
+
+    for i in range(len(statematrix)):
+        state = statematrix[i]
+        new_time_slice = []
+        for j in range(len(state)):
+            new_feature_state = [0]*feature_vector_length
+            # position (1)
+            new_feature_state[0] = j / feature_vector_length
+
+            # pitch class (12)
+            new_feature_state[j % 12 + 1] += 1
+
+            # previous vicinity (50)
+            # this occupies the space [13, 63)
+            for k in range(-12, 13):
+                if -1 < j + k < feature_vector_length:
+                    prev_note = state[j + k]
+                    context_index = (k + 12) * 2 + 13
+                    new_feature_state[context_index] += prev_note[0]
+                    new_feature_state[context_index + 1] += prev_note[1]
+
+            # previous context (12)
+            tally = [0] * 12
+            for k in range(len(state)):
+                tally[k % 12] += state[k][0]
+            for k in range(12):
+                new_feature_state[k + 63] += tally[k] # maybe normalize this?
+
+            # beat
+            new_feature_state[75 + i % 4] ++ 1
+            new_time_slice.append(new_feature_state)
+        new_feature_matrix.append(new_time_slice)
+    return new_feature_matrix # convert to numpy array?
+    # this method is probably bugged. TODO: fix.
