745ca59a2fd1e74fbc4800fdcd09361a27b61e2f
Alexander Yang <yangalexandery@gmail.com>
2017-10-14 07:27:17 +0000

    more minor changes

--- previous_work_model.py
+++ previous_work_model.py
@@ -42,17 +42,12 @@ note_state_2 = tf.zeros([batch_size * sample_length, note_lstm_state_size_2])
 W = tf.Variable(np.random.rand(note_lstm_state_size_2, num_output_categories), dtype=tf.float32) # weights for state t-1 -> state t
 b = tf.Variable(np.zeros((1, num_output_categories)), dtype=tf.float32)
 
-# time_layer_input = tf.unstack(batchX_placeholder, axis=1)
 time_layer_1_input = tf.transpose(batchX_placeholder, [0, 2, 1, 3])
 time_layer_1_input = tf.reshape(time_layer_1_input, [batch_size * note_size, sample_length, attr_size])
-# print(time_layer_input.shape)
-# time_layer_input = tf.transpose(batchX_placeholder, [0, 1, 2, 3])
 
 time_layer_1_output, time_state = tf.nn.dynamic_rnn(cell=time_lstm, inputs=time_layer_1_input, dtype=tf.float32)
 
 # time_layer_output shape is [batch_size * note_size, sample_length, time_lstm_state_size]
-# time_layer_1_output = tf.reshape(time_layer_1_output, [batch_size, note_size, sample_length, time_lstm_state_size])
-
 
 with tf.variable_scope('time_scope_2') as scope:
     time_layer_2_output, time_state_2 = tf.nn.dynamic_rnn(cell=time_lstm_2, inputs = time_layer_1_output, dtype=tf.float32)
@@ -66,9 +61,7 @@ transpose_layer_output = tf.transpose(time_layer_2_output, [0, 2, 1, 3])
 transpose_layer_output = tf.reshape(transpose_layer_output, [batch_size * sample_length, note_size, time_lstm_state_size])
 
 with tf.variable_scope('note_scope') as scope:
-	note_layer_1_output, note_state = tf.nn.dynamic_rnn(cell=note_lstm, inputs=transpose_layer_output, dtype=tf.float32)
-
-# note_layer_1_output = tf.reshape(note_layer_output, [batch_size * sample_length * note_size, note_lstm_state_size])
+    note_layer_1_output, note_state = tf.nn.dynamic_rnn(cell=note_lstm, inputs=transpose_layer_output, dtype=tf.float32)
 
 
 with tf.variable_scope('note_scope_2') as scope:
@@ -81,27 +74,19 @@ note_layer_2_output = tf.reshape(note_layer_2_output, [batch_size * sample_lengt
 
 # batchY_placeholder_transposed = tf.transpose(batchY_placeholder, [2, 0, 1])
 output_series = tf.cast(tf.reshape(batchY_placeholder, [batch_size * sample_length * note_size * num_output_categories]), tf.float32)
+output_series_0, output_series_1 = tf.split(output_series, 2, 0)
 
 # weights = tf.Variable([[1.0, 0.0, 0.0], [0.0, 2.0, 0.0], [0.0, 0.0, 10.0]], tf.float32)
 # logits_series = tf.matmul(tf.matmul(note_layer_output, W) + b, weights)
 logits_series = tf.matmul(note_layer_2_output, W) + b
 sigmoid_output = tf.sigmoid(tf.reshape(logits_series, [batch_size * sample_length * note_size * num_output_categories]))
+sigmoid_output_0, sigmoid_output_1 = tf.split(sigmoid_output, 2, 0)
 
 predictions_series = tf.reshape(sigmoid_output, [batch_size, sample_length, note_size, num_output_categories])
-losses = -tf.log(2 * tf.multiply(sigmoid_output, output_series) - sigmoid_output - output_series + tf.fill([batch_size * sample_length * note_size * num_output_categories], 1.0))
+losses = -tf.log(2 * tf.multiply(sigmoid_output_0, output_series_0) - sigmoid_output_0 - output_series_0 + tf.fill([batch_size * sample_length * note_size], 1.0))
 # losses = []
 # predictions_series = tf.reshape(tf.nn.softmax(logits_series), [batch_size, sample_length, note_size, num_output_categories])
 
-
-# losses.append(tf.nn.sparse_softmax_cross_entropy_with_logits(
-# 	logits=logits_series,
-# 	labels=output_series
-# ))
-# for logits, labels in zip(logits_series, output_series):
-#     losses.append(tf.nn.sparse_softmax_cross_entropy_with_logits(
-#         logits=logits,
-#         labels=labels
-#     ))
 
 total_loss = tf.reduce_sum(losses)
 train_step = tf.train.AdadeltaOptimizer(learning_rate=0.01, rho=0.95, epsilon=0.000001).minimize(total_loss)
@@ -121,21 +106,15 @@ def train_model():
             y = []
             # print(pieces[0][0].shape)
             for i in range(batch_size):
-            	rand_sample = model_helpers.get_random_sample(pieces, sample_length)
-            	x.append(model_helpers.transform_statematrix(rand_sample))
-            	# y.append(model_helpers.get_transformed_output(rand_sample))
+                rand_sample = model_helpers.get_random_sample(pieces, sample_length)
+                x.append(model_helpers.transform_statematrix(rand_sample))
+                # y.append(model_helpers.get_transformed_output(rand_sample))
                 # x.append(model_helpers.get_random_sample(pieces, sample_length))
                 y.append(model_helpers.get_sample_output(rand_sample))
             x = np.array(x)
             y = np.array(y)
             # x = np.array(get_random_sample(pieces))
             # y = np.array(get_sample_output(x))
-            # x = np.expand_dims(x, axis=0)
-            # y = np.expand_dims(y, axis=0)
-            # x = np.expand_dims(np.array(get_random_sample(pieces)), axis=0)
-            # y = np.expand_dims(np.array(get_sample_output(x)), axis=0)
-            # np.expand_dims(y, axis=0)
-            # print(x.shape)
 
             _total_loss, _train_step, _predictions_series = sess.run(
                 [total_loss, train_step, predictions_series], 
